43f488fc583d54db431cdd6dad601bf93cf85751
liam <zhuchanglizq@126.com>
2022-03-21 11:33:44 +0800

    寻找字符串数组中，公共开头的字符串

---
+++ src/liam/year22/march/w4/longCommonPrefix/Main.java
@@ -0,0 +1,14 @@
+package liam.year22.march.w4.longCommonPrefix;
+
+/**
+ * @Description
+ * @Author liam661
+ * @Date 2022/3/21 11:13
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String [] strArr = {"abwecde", "abcert", "abcuiop"};
+        System.out.println(s.longCommonPrefix(strArr));
+    }
+}
---
+++ src/liam/year22/march/w4/longCommonPrefix/Solution.java
@@ -0,0 +1,38 @@
+package liam.year22.march.w4.longCommonPrefix;
+
+/**
+ * @Description
+ * 横向比较法
+ * @Author liam661
+ * @Date 2022/3/21 10:47
+ **/
+public class Solution {
+    public String longCommonPrefix(String [] strs){
+        String prefix = strs[0];
+        int len = strs.length;
+        // 从第一个开始横向比较，拿公共开头的字符串和后面的继续比较
+        for(int i = 1; i < len; i++){
+            prefix = longCommonPrefix(prefix, strs[i]);
+            if(prefix.length() == 0){
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    /**
+     * 比较两个字符串，获取开头的公共字符串
+     * @param str1
+     * @param str2
+     * @return 开头的公共字符串
+     */
+    public String longCommonPrefix(String str1, String str2){
+        // 只需要比较到公共长度即可，需要找到两个字符串的最短长度
+        int len = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < len && str1.charAt(index) == str2.charAt(index)){
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
